b5522e232a198a9f8cc69abcd84168aa0c35d716
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-06-04 19:09:29 +0400

    feat: add lang folder add localization middleware  add database setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,10 +12,11 @@ return new class () extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->boolean('is_google')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
