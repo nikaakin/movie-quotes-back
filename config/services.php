c0df0a88351c36f807365d9e73eccc71ab22256b
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-11 11:53:24 +0400

    refactor: seed database with right text

--- config/services.php
+++ config/services.php
@@ -17,7 +17,10 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:3000/',
+        'redirect' => env(
+            'FRONTEND_URL',
+            'http://localhost:3000/'
+        ),
     ],
 
     'mailgun' => [
