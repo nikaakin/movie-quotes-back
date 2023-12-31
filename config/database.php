37f52a8a3888e8b2c8c7508493ff8f58b4aa39ce
Nika Cuckiridze <nikacuckiridze47@gmail.com>
2024-01-01 01:53:55 +0400

    feat: change pgsql config

--- config/database.php
+++ config/database.php
@@ -76,7 +76,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
-            'options' => [],
         ],
 
         'sqlsrv' => [
