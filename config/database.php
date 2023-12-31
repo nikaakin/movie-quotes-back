01326acf3802a19ff1dc8a3dd3f59306022d20fb
Nika Cuckiridze <nikacuckiridze47@gmail.com>
2024-01-01 01:17:17 +0400

    feat: change pgsql config

--- config/database.php
+++ config/database.php
@@ -76,9 +76,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'false',
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [],
         ],
 
         'sqlsrv' => [
